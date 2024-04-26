171291613dd7227c70b0a62d32276e9752025fca
unknown <empregos97@gmail.com>
2024-04-26 09:13:26 -0300

    SEO development

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,16 +14,25 @@ export const metadata: Metadata = {
 
   metadataBase: new URL("https://catalogojmluzearte.vercel.app/"),
   keywords: keywords,
+  icons: "/favicon.ico",
   openGraph: {
-    description:
-      "Produtos de útilidades , decoração , iluminação e muito mais voçe encontra aqui  ",
-    images: ["https://github-production-user-asset-6210df.s3.amazonaws.com/80254945/325795605-e9b6246b-b9f3-40a2-99a8-bfe0076dea04.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240425%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240425T231805Z&X-Amz-Expires=300&X-Amz-Signature=d3b913637a764bbf6043c6602629c2da5243f87e0dfb574b426d42d0da3fbd30&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=755925151"],
-  },
-  icons: "https://images.tcdn.com.br/img/img_prod/1112215/1652709121_icone-_2_9_1.ico?662aba504c5f8"
+    emails: "dageltubos@gmail.com",
+    type: "website",
+    locale: "pt_BR",
 
 
-};
+    images: [
+      {
+        url: "https://github.com/97revenge/jsdom-trial/assets/80254945/28fc654d-a943-440d-ba40-f65786dc02b2",
+        width: 830,
+        height: 630,
+        alt: "Counpon Image for new clientes",
+      },
 
+    ],
+
+  }
+}
 export default function RootLayout({
   children,
 }: Readonly<{
