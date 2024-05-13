45f85bbb8fc188072351eb5ee77a5c48059ec167
unknown <empregos97@gmail.com>
2024-05-13 09:20:00 -0300

    newsletter off

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,7 +1,5 @@
 import { prisma } from "@/lib/prisma/prisma";
 
 export const GET = async () => {
-  const data = await prisma.subscribers.findMany();
-
-  return Response.json(data);
+  return Response.json([]);
 };
