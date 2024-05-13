5224ad68a9ab70cadcdd6a4681ffb02fee997c2f
unknown <empregos97@gmail.com>
2024-05-13 10:24:10 -0300

    useTransition handling fetch

---
+++ components/Loaders/NeutronLoader.tsx
@@ -0,0 +1,21 @@
+import { TextAnimatedGradient } from "../component/text-gradient";
+
+export const NeutronLoader = () => {
+  return (
+    <>
+      <div>
+        <div id="page">
+          <div id="container">
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="ring"></div>
+            <div id="h3" className="text-4xl">
+              💡
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
