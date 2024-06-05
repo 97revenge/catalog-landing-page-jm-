c6ec7b6e797285bbbe4598af4906317e16612327
unknown <empregos97@gmail.com>
2024-06-05 16:22:26 -0300

    implemeting new system for design prompt

---
+++ lib/helpers.ts
@@ -0,0 +1,25 @@
+export namespace Helpers {
+
+
+  export const businessData = {
+    address: "Endereço: R. Lucinda Gomes Barreto, 726 - Vila Nova Manchester, São Paulo - SP, 03440-040",
+    tellphone: 'Telefone: (11) 99276-1928',
+    openningHours: `quarta-feira
+quinta-feira	0800-18:00
+sexta-feira
+sábado	Fechado
+domingo	Fechado
+segunda-feira`,
+
+    categorys: `Iluminação
+Luminárias
+Tomadas Elétricas
+rabichos`,
+    Description: `A JM Luz e arte transforma e desenvolve ideias em realidade, customizando cabos e luminárias em peças decorativas. O que proporciona uma experiência positiva aos nosso clientes`
+  }
+
+
+  export const initialQuestion = (data: any) => `me de informações iniciais para um cliente sobre esta empresa chamada JM luz e arte : ${data}`
+
+}
+
