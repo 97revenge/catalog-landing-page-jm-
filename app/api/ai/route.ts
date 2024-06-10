d535c306ab9ab01ea81d7d9f4ff052217f2b0e34
unknown <empregos97@gmail.com>
2024-06-10 16:31:10 -0300

    implementation of conversational chat by server actions

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -1,35 +1,40 @@
-import { Helpers } from "@/lib/helpers";
-import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { StreamingTextResponse, streamText } from "ai";
-import { NextRequest } from "next/server";
+// import { Helpers } from "@/lib/helpers";
+// import { createGoogleGenerativeAI } from "@ai-sdk/google";
+// import { StreamingTextResponse, streamText } from "ai";
+// import { NextRequest } from "next/server";
 
-export const system = `Você é um assistente útil executado dentro de um catálogo de produtos de uma empresa de Abajures e Luminárias chamada JM Luz e Arte, entregue apenas informações baseadas nesta empresa e como ajudar os clientes a comprar seus produtos ou entrar em contato com o suporte da empresa. use essas informações sobre a empresa para ter mais embasamento nas perguntas :
-      ${Helpers.businessData.content}
-      ${Helpers.businessData.Description}
-      ${Helpers.businessData.address}
-      ${Helpers.businessData.tellphone}
-      ${Helpers.businessData.categorys}
-      ${Helpers.businessData.openningHours}
-      ${Helpers.businessData.products}
-      `
+// export const system = `Você é um assistente útil executado dentro de um catálogo de produtos de uma empresa de Abajures e Luminárias chamada JM Luz e Arte, entregue apenas informações baseadas nesta empresa e como ajudar os clientes a comprar seus produtos ou entrar em contato com o suporte da empresa. use essas informações sobre a empresa para ter mais embasamento nas perguntas :
+//       ${Helpers.businessData.content}
+//       ${Helpers.businessData.Description}
+//       ${Helpers.businessData.address}
+//       ${Helpers.businessData.tellphone}
+//       ${Helpers.businessData.categorys}
+//       ${Helpers.businessData.openningHours}
+//       ${Helpers.businessData.products}
+//       `
 
-const google = createGoogleGenerativeAI({
-  apiKey: process.env.GEMINI_API_KEY!,
-});
+// const google = createGoogleGenerativeAI({
+//   apiKey: process.env.GEMINI_API_KEY!,
+// });
 
-export const POST = async (req: NextRequest) => {
-  try {
-    const { messages } = await req.json();
+// export const POST = async (req: NextRequest) => {
+//   try {
+//     const { messages } = await req.json();
 
-    const result = await streamText({
-      model: google("models/gemini-1.5-pro-latest"),
-      system,
-      messages,
-    });
+//     const result = await streamText({
+//       model: google("models/gemini-1.5-pro-latest"),
+//       system,
+//       messages,
+//     });
 
-    return new StreamingTextResponse(result.toAIStream());
-  } catch (error) {
-    console.log(error);
-    return Response.json(error);
-  }
-};
+//     return new StreamingTextResponse(result.toAIStream());
+//   } catch (error) {
+//     console.log(error);
+//     return Response.json(error);
+//   }
+// };
+
+
+export const GET = async () => {
+  return Response.json({ status: 200 })
+}
